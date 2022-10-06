09dbe4fc242617e7abb87d6850af2595b85e3b25
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-10-06 06:10:47 -0700

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
     if str == "":
-        print("")
-    up_str = ""
-    for i in str:
-        x = ord(i)
-        if x in range(97, 123):
-            up_str += chr(x - 32)
-        else:
-            up_str += i
-    print("{}".format(up_str))
+        pass
+    else:
+        up_str = ""
+        for i in str:
+            x = ord(i)
+            if x in range(97, 123):
+                up_str += chr(x - 32)
+            else:
+                up_str += i
+        print("{}".format(up_str))
