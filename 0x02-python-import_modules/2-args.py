46fc0ef31883713340d217b6a8034e9bf649ccd8
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-10-13 10:35:52 -0700

    Project 0x02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def arguments():
+    ar = sys.argv
+    ar_len = len(ar) - 1
+    
+    if ar_len == 0:
+        print(f"{ar_len} arguments.")
+    elif ar_len == 1:
+        print(f"{ar_len} argument:")
+        print(f"1: {ar[1]}")
+    else:
+        print(f"{ar_len} arguments:")
+        for x in range(1, ar_len + 1):
+            print(f"{x}: {ar[x]}")
+
+if __name__ == "__main__":
+    arguments()
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def inf_add():
+    ar = sys.argv
+    ar_len = len(ar)
+    _add = 0
+
+    if ar_len < 2:
+        pass
+    else:
+        for x in range(1, ar_len):
+            _add += int(ar[x])
+    print(_add)
+
+if __name__ == "__main__":
+    inf_add()
